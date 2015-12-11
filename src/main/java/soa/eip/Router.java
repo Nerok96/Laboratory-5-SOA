83c3e3ef607a9521222e6a596ee6c02f41e383a8
Jorge <nerok96@gmail.com>
2015-12-11 21:56:25 +0100

    Done all the exchanges needed

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -9,10 +9,10 @@ public class Router extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("direct:search")
-			.to("twitter://search?consumerKey={{twitter.consumerKey}}&"
-					+ "consumerSecret={{twitter.consumerSecret}}&"
-					+ "accessToken={{twitter.accessToken}}&"
-					+ "accessTokenSecret={{twitter.accessTokenSecret}}");
+			.to("twitter://search?consumerKey=Rs3hb4qSYa9feoSQK0SMhvUXc&"
+					+ "consumerSecret=DGMKnwYLdf8D4OaJyWgrZbP6gLP7FvirPrFq4xyITOpxCfgQ52&"
+					+ "accessToken=1732307449-NB0Q9X2W1cSggDWV8pyuuwqQPVXZQzvLIIdVqQE&"
+					+ "accessTokenSecret=NezykxsHge3Ywm9rEgTZIWghuQJ5VSLhZ1YLAKA2rtOOs");
 	}
 
 }
--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -23,6 +23,18 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
+		int i = q.toLowerCase().indexOf(" max:");
+        HashMap<String, Object> m =  new HashMap<String, Object>();
+        if(i >= 0){
+            String q1 = q.substring(0, i);
+            String q2 = q.substring(i+5,q.length());
+            System.out.println(q1 + "-" + q2);
+            m.put("CamelTwitterKeywords",q1);
+            m.put("CamelTwitterCount",q2);
+        }else{
+            m.put("CamelTwitterKeywords",q);
+        }
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", m);
         return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
     }
 }
